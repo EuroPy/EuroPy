ceaab023dd0d2d188cad18a4d77862d87f21d3f9
Matthew Alvarez <matt@mattjalvarez.com>
2020-11-27 19:11:07 -0600

    tweak capturing

--- europy/lifecycle/reporting.py
+++ europy/lifecycle/reporting.py
@@ -1,21 +1,19 @@
 import os
 from typing import Dict
-from typing import Union, List
+from typing import List
 
-from matplotlib import pyplot
 from pandas import DataFrame
 
 from europy import report_directory
 from europy.lifecycle.model_details import ModelDetails
 from europy.lifecycle.report import Report
-from europy.lifecycle.report_figure import ReportFigure
-from europy.lifecycle.result import TestResult, TestLabel
+from europy.lifecycle.result import TestResult
 from europy.lifecycle.result_promise import TestPromise
 from europy.utils import isnotebook
 
 tests: dict = dict()
 
-report = Report()
+__report = Report()
 
 
 def put_test(promise: TestPromise):
@@ -28,34 +26,13 @@ def put_test(promise: TestPromise):
         tests[key] = promise
 
 
-def get_report() -> Report:
-    return report
-
-
-def capture(key: str,
-            labels: List[Union[str, TestLabel]],
-            result: Union[float, str, bool, DataFrame, TestResult],
-            figures: [ReportFigure] = [],
-            description: str = None,
-            success: bool = None) -> TestResult:
-    test_result = TestResult(key, labels, result, figures, description, success)
-    report.capture(test_result)
-    return test_result
-
-
 def capture_model_details(details: ModelDetails):
-    report.model_card['details'] = details
+    __report.model_card['details'] = details
 
 
 def capture_parameters(name: str, params: Dict):
     # combine parameters
-    report.model_card['parameters'][name] = {**report.model_card['parameters'].get(name, {}), **params}
-
-
-def capture_figure(name: str, plot: pyplot):
-    metadata = ReportFigure.of(name, plot)
-
-    report.figures.append(metadata)
+    __report.model_card['parameters'][name] = {**__report.model_card['parameters'].get(name, {}), **params}
 
 
 def make_report_dir():
@@ -68,9 +45,19 @@ def make_report_dir():
 
 
 def execute_tests(*args, **kwargs):
+    report = Report()
     make_report_dir()
     test_results: List[TestResult] = [tests[key].execute(*args, **kwargs) for key in tests.copy()]
     test_result_df = DataFrame([x.__dict__ for x in test_results])
+
+    for result in test_results:
+        report.capture(result)
+        for figure in result.figures:
+            report.figures.append(figure)
+
+    ## TODO: The model card and params should come from some test results
+    ## Then those should be combined into the report itself
+    report.model_card = __report.model_card
 
     passing_count = len(list(filter(lambda x: x.success, test_results)))
     failing_count = len(list(filter(lambda x: not x.success, test_results)))
